e55a9f694fa5de9d685cf19c143e812a65365d87
het-patel99 <hetpatel0199@gmail.com>
2024-02-12 22:34:48 -0500

    changes

--- kzntree/urls.py
+++ kzntree/urls.py
@@ -39,5 +39,5 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
     path('admin/', admin.site.urls),
-    path('/', include('kzntreeDashboard.urls')),
+    path('', include('kzntreeDashboard.urls')),
 ]

daa5817eaf4f35a22f6ac9701c080372ee78d98a
Ubuntu <ubuntu@ip-172-31-32-151.ec2.internal>
2024-02-13 05:31:30 +0000

    changes

--- kzntree/settings.py
+++ kzntree/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5#9g5tg^r!yw4ja3=c&q%j)7#wjgwjgcr#!$anr7fmao0olyr3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.207.29.118']
 
 
 # Application definition
